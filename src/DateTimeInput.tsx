0e09fba7ff76a4a77bb5ca1f2aa0941568307e6d
Katsushi OUGI <ougi@to-r.net>
2024-03-05 19:22:28 +0900

    viteのライブラリビルド設定

---
+++ src/DateTimeInput.tsx
@@ -0,0 +1,10 @@
+function DateTimeInput() {
+  return (
+    <div>
+      <input type="date" />
+      <input type="time" />
+    </div>
+  );
+}
+
+export default DateTimeInput;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/DateTimeInput.tsx"),
+      name: "DateTimeInput",
+      fileName: "datetime-input",
+    },
+    rollupOptions: {
+      external: ["react"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
   plugins: [react()],
-})
+});
